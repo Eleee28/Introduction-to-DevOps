5f44e0fcc7c4cee97eef558e714dd800b55359b3
Eleee28 <elena.juarrosgonzalez@gmail.com>
2025-02-20 14:04:27 +0000

    lab 2 code added

---
+++ labs/lab2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("This is another string!\n");
+
+    for (int i = 1; i < 11; i++) 
+        printf("%d ", i);
+    printf("\n");
+
+    return (0);
+}
